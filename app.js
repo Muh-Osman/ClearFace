e49dfbc659721043eb649ec34d303b78839c6f99
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-11-02 12:40:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+document.querySelector('.x193iq5w.xgmub6v.x1ceravr').style.display = 'none';
+document.querySelector('.xz9dl7a.x1swvt13.x1pi30zi.x1a8lsjc.x1a02dak.x78zum5.x6s0dn4').style.display = 'none';
+document.querySelector('.x1ja2u2z.xh8yej3.x1n2onr6.xjkvuk6.x1iorvi4').style.display = 'none';
+document.querySelector('.xb57i2i.x1q594ok.x5lxg6s.x78zum5.xdt5ytf.x6ikm8r.x1ja2u2z.x1pq812k.x1rohswg.xfk6m8.x1yqm8si.xjx87ck.x1l7klhg.x1iyjqo2.xs83m0k.x2lwn1j.xx8ngbg.xwo3gff.x1oyok0e.x1odjw0f.x1e4zzel.x1n2onr6.xq1qtft').style.display = 'none';
+document.querySelector('.x1n2onr6.x1ja2u2z.x9f619.x78zum5.xdt5ytf.x2lah0s.x193iq5w.xjkvuk6.x1cnzs8').style.display = 'none';
+document.querySelector('.x13fuv20.x26u7qi.xu3j5b3.x1q0q8m5.x972fbf.xm0m39n.xcfux6l.x1qhh985.x9f619.x78zum5.x1iyjqo2.xs83m0k.x1qughib.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xeuugli.xexx8yu.x18d9i69.x1n2onr6.x1ja2u2z.x6s0dn4.x1q0g3np.x1sxyh0.xurb0ha.x1gg8mnh').style.display = 'none';
+document.querySelector('.x1n2onr6.x1ja2u2z.x9f619.x78zum5.xdt5ytf.x2lah0s.x193iq5w.xjkvuk6.x1cnzs8').style.display = 'none';
+document.querySelector('.x13fuv20.x26u7qi.xu3j5b3.x1q0q8m5.x972fbf.xm0m39n.xcfux6l.x1qhh985.x9f619.x78zum5.x1iyjqo2.xs83m0k.x1qughib.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xeuugli.xexx8yu.x18d9i69.x1n2onr6.x1ja2u2z.x6s0dn4.x1q0g3np.x1sxyh0.xurb0ha.x1gg8mnh').style.display = 'none';
+document.querySelector('.x13fuv20.x26u7qi.xu3j5b3.x1q0q8m5.x972fbf.xm0m39n.xcfux6l.x1qhh985.x9f619.x78zum5.x1iyjqo2.xs83m0k.x1qughib.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xeuugli.xexx8yu.x18d9i69.x1n2onr6.x1ja2u2z.x6s0dn4.x1q0g3np.x1sxyh0.xurb0ha.x1gg8mnh').style.display = 'none';
+document.querySelector('a.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.xc73u3c.x5ib6vp.x1y1aw1k.xwib8y2').style.display = 'none';
+document.querySelector('.x1n2onr6.x1ja2u2z.x9f619.x78zum5.xdt5ytf.x2lah0s.x193iq5w.xwib8y2.xyamay9').style.display = 'none';
+document.querySelector('.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.x11i5rnm.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.xmns6w2.xpkgp8e.x1iorvi4.xjkvuk6.xr9ek0c.xjpr12u').style.display = 'none';
+document.querySelector('a.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.x1ou2tus.x1egjynq.x1y1aw1k.xwib8y2').style.display = 'none';
+document.querySelector('a.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.x1ou2tus.x1egjynq.x1y1aw1k.xwib8y2').style.display = 'none';
+document.querySelector('a.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.x1ou2tus.x1egjynq.x1y1aw1k.xwib8y2').style.display = 'none';
+document.querySelector('.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.x11i5rnm.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.xmns6w2.xpkgp8e.x1iorvi4.xjkvuk6.xr9ek0c.xjpr12u').style.display = 'none';
+document.querySelector('.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.xmns6w2.xpkgp8e.x1iorvi4.xjkvuk6.xjpr12u').style.display = 'none';
+document.querySelector('.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x2lah0s.x193iq5w.x1cy8zhl.xwib8y2.x1y1aw1k').style.display = 'none';
+document.querySelector('.x1i10hfl.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.x11i5rnm.x1mh8g0r.x2lwn1j.xeuugli.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x78zum5.xmns6w2.xpkgp8e.x1iorvi4.xjkvuk6.xr9ek0c.xjpr12u').style.display = 'none';
+document.querySelector('.x78zum5.x1nhvcw1.x1n2onr6.xh8yej3').style.display = 'none';
+document.querySelector('span.x4k7w5x.x1h91t0o.x1h9r5lt.x1jfb8zj.xv2umb2.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1qrby5j').style.display = 'none';
+document.querySelector('.x1mdubkq.x1swvt13.x1l90r2v.x1pi30zi.xyamay9').style.display = 'none';
+// document.querySelector('').style.display = 'none';
+
+
+
+
